db51663af492aedde7181319948033c59f3dc4c4
simon pineda <simonpineda0521@gmail.com>
2024-07-02 17:28:42 -0500

    The errors heanding was added

--- src/functions.ts
+++ src/functions.ts
@@ -13,58 +13,73 @@ async function ChangeKey1(TheDoc: any, array: Array<string | undefined>) {
 }
 
 export async function CallNewsHead(category: string, contry: string, q: string, prioritydomain: string, page: number): Promise<any> {
+
+  let ERROR = undefined
+
   let ListKeys1 = await getDoc(itemsColection2).then((snap) => {
     if (snap.exists()) {
       return snap.data().All
     }
+  }).catch(() => {
+    ERROR = 'Error to get the keys'
   })
-  let response: any = await fetch(`https://newsdata.io/api/1/news?apikey=${ListKeys1[0]}&size=9&language=en${category !== '' ? `&category=${category}` : ''}${q !== '' ? `&q=${q}` : ''}${contry !== '' ? `&country=${contry}` : ''}${page !== 1 ? `&page=${page}` : ''}${prioritydomain !== '' ? `&prioritydomain=${prioritydomain}` : ''}`)
-  response = await response.json()
-  if (response.status === 'error') {
+  const response: any = await fetch(`https://newsdata.io/api/1/news?apikey=${ListKeys1[0]}&size=9&language=en${category !== '' ? `&category=${category}` : ''}${q !== '' ? `&q=${q}` : ''}${contry !== '' ? `&country=${contry}` : ''}${page !== 1 ? `&page=${page}` : ''}${prioritydomain !== '' ? `&prioritydomain=${prioritydomain}` : ''}`).then((response) => response.json()).catch(() => {
+    ERROR = 'Error to get the news'
+    return { status: 'error', totalResults: 0, nextPage: '', results: [] }
+  })
+
+  if (response.status === 'error' || !!ERROR) {
     await ChangeKey1(itemsColection2, ListKeys1)
-    return await CallNewsHead(category, contry, q, prioritydomain, page)
+    return await { result: response, error: ERROR }
   }
+
   let ListKeys2 = await getDoc(itemsColection).then((snap) => {
     if (snap.exists()) {
       return snap.data().All
     }
-  })
-
-
-  let ERROR = undefined
-
+  }).catch(() => [])
 
   await Promise.all(
     response.results.map(async (not: any) => {
 
 
-      if (ListKeys2[0] === "TEST") {
+      const AfterJson = await fetch(`https://api.worldnewsapi.com/extract-news?analyze=true&url=${not.link}&api-key=${ListKeys2[0]}`).then(res => res.json()).catch(() => { ERROR = 'Cannot acces to the content API'; return undefined })
 
-        if (!!not.description) {
+      if (ListKeys2[0] === "TEST" || AfterJson === undefined) {
+        if (not.description !== '' && not.description !== 'ONLY AVAILABLE IN PAID PLANS') {
           const responseIA = await fetch('https://fact-finder-api.onrender.com/predict', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-              title: (removeStopwords(not.title.toLowerCase().split(' '))).join(' '),
-              text: (removeStopwords(not.description.toLowerCase().split(' '))).join(' ')
+              title: (removeStopwords(not.title?.toLowerCase().split(' '))).join(' '),
+              text: (removeStopwords(not.description?.toLowerCase().split(' '))).join(' ')
             })
+          }).then(res => res.json()).catch(() => {
+            ERROR = 'Cannot acces to the IA model'
+            return undefined
           })
-          const afertJson = await responseIA.json()
-          not.falsity = await afertJson.FakePosibility
-          not.polarization = await 1 - afertJson.Polarity.neu
+          if (responseIA !== undefined) {
+            not.falsity = await responseIA.FakePosibility
+            not.polarization = await 1 - responseIA.Polarity.neu
+          }
+          else {
+            not.falsity = NaN
+            not.polarization = NaN
+          }
         }
         else {
-          not.polarization = await Math.round(Math.random() * 100) / 100
-          not.falsity = await Math.round(Math.random() * 100) / 100
+
+          ERROR = 'Cannot get the news content'
+          not.polarization = NaN
+          not.falsity = NaN
         }
 
         not.content = not.description
         return await not
       }
-      const second = await fetch(`https://api.worldnewsapi.com/extract-news?analyze=true&url=${not.link}&api-key=${ListKeys2[0]}`)
-      const AfterJson = await second.json()
+
       if (AfterJson.status === "failure" && AfterJson.code === 402) {
         await ChangeKey1(itemsColection, ListKeys2)
         not.content = await not.description
@@ -84,20 +99,24 @@ export async function CallNewsHead(category: string, contry: string, q: string, 
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            title: (removeStopwords(not.title.toLowerCase().split(' '))).join(' '),
-            text: (removeStopwords(not.content.toLowerCase().split(' '))).join(' ')
+            title: (removeStopwords(not.title?.toLowerCase().split(' '))).join(' '),
+            text: (removeStopwords(not.description?.toLowerCase().split(' '))).join(' ')
           })
-        });
-
-        const afertJson = await responseIA.json()
-        not.polarization = await AfterJson.sentiment ? (Math.round(AfterJson.sentiment * 100) / 100) : 1 - afertJson.Polarity.neu
-        not.falsity = await afertJson.FakePosibility
+        }).then(res => res.json()).catch(() => {
+          ERROR = 'Cannot acces to the IA model'
+          return undefined
+        })
+        if (responseIA) {
+          not.polarization = await  1 - responseIA.Polarity.neu
+          not.falsity = await responseIA.FakePosibility
+        }
+        else{
+          not.polarization = NaN
+          not.falsity = NaN
+        }
       }
       return await not
-
     })
-  ).catch(() => {
-    ERROR = 'Cannot load the news content'
-  })
+  )
   return { result: response, error: ERROR }
 }
